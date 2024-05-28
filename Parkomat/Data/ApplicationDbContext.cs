ed49fe1359dea40287ed526855a2b788eb9266be
MaciejGlowienka <maciek.glowienka@gmail.com>
2024-05-28 15:08:32 +0200

    Basic configuration

---
+++ Parkomat/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parkomat.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
